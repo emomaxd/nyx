4fb2680dcedb7a1473dbf0eb71d2701e19c50a0e
EmomaxD <emreleno@gmail.com>
2025-04-12 21:51:14 +0300

    Implement custom fast sqrt approximation

---
+++ include/math/math.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cstdint>
+
+#include "core/base.h"
+
+namespace nyx {
+    NYX_FORCEINLINE real_t sqrt(real_t x)
+    {
+        /* Edge case for both data types */
+        if (x <= (real_t)0.0) return (real_t)0.0;
+
+        #ifdef USE_DOUBLE_PRECISION
+        /* DOUBLE implementation */
+        const size_t magic = 0x5fe6eb50c7b537a9;
+        #else
+        /* FLOAT implementation*/
+        const size_t magic = 0x5f3759df;
+        #endif
+        
+        union {
+            real_t F;
+            uint32_t I;
+        } u = { x };
+
+        u.I = magic - (u.I >> 1);
+        real_t y = u.F;
+        
+        real_t xhalf = 0.5f * x;
+        y = y * (1.5f - xhalf * y * y);
+
+        real_t sqrtX = x * y;
+
+        /* OPTIONAL 
+        * TODO: Test the performance and accuracy with and without this line.
+        */
+        sqrtX = sqrtX - ((sqrtX * sqrtX - x) / (2.0f * sqrtX));
+
+        return sqrtX;        
+    }
+} // namespace nyx
