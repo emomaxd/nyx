2a043dba735d53ebe303d5b0a42b057e498f7dda
EmomaxD <emreleno@gmail.com>
2025-05-01 23:18:12 +0300

    Change rb integration from verlet to impulse based integration.

--- include/nyx/physics/rigidbody/rigidbody_system.h
+++ include/nyx/physics/rigidbody/rigidbody_system.h
@@ -5,6 +5,8 @@
 
 #include "nyx/core/base.h"
 #include "nyx/math/vec3.h"
+#include "nyx/math/quaternion.h"
+#include "nyx/math/mat3.h"
 
 namespace nyx {
 
@@ -13,27 +15,29 @@ struct RigidbodyData {
     ~RigidbodyData() = default;
 
     NYX_FORCEINLINE const std::vector<Vec3>& getPositions() const { return Positions; }
-    NYX_FORCEINLINE const std::vector<Vec3>& getPrevPositions() const { return PrevPositions; }
     NYX_FORCEINLINE const std::vector<Vec3>& getVelocities() const { return Velocities; }
-    NYX_FORCEINLINE const std::vector<Vec3>& getForces() const { return Forces; }
-    NYX_FORCEINLINE const std::vector<real_t>& getMasses() const { return Masses; }
-    NYX_FORCEINLINE const std::vector<real_t>& getInvMasses() const { return InvMasses; }
-    NYX_FORCEINLINE const std::vector<uint32_t>& getActiveFlags() const { return Active; }
+    NYX_FORCEINLINE const std::vector<Vec3>& getAngularVelocities() const { return AngularVelocities; }
+    NYX_FORCEINLINE const std::vector<Quaternion>& getOrientations() const { return Orientations; }
+    NYX_FORCEINLINE const std::vector<Mat3>& getInvInertias() const { return InvInertias; }
 
     NYX_FORCEINLINE std::vector<Vec3>& accessPositions() { return Positions; }
-    NYX_FORCEINLINE std::vector<Vec3>& accessPrevPositions() { return PrevPositions; }
     NYX_FORCEINLINE std::vector<Vec3>& accessVelocities() { return Velocities; }
-    NYX_FORCEINLINE std::vector<Vec3>& accessForces() { return Forces; }
+    NYX_FORCEINLINE std::vector<Vec3>& accessAngularVelocities() { return AngularVelocities; }
+    NYX_FORCEINLINE std::vector<Quaternion>& accessOrientations() { return Orientations; }
 
 private:
     static constexpr size_t kInitialEntityCount = 10'000;
 
     NYX_ALIGNAS_CACHE std::vector<Vec3> Positions;
-    NYX_ALIGNAS_CACHE std::vector<Vec3> PrevPositions;
     NYX_ALIGNAS_CACHE std::vector<Vec3> Velocities;
-    NYX_ALIGNAS_CACHE std::vector<Vec3> Forces;
+    NYX_ALIGNAS_CACHE std::vector<Vec3> AngularVelocities;
+    NYX_ALIGNAS_CACHE std::vector<Quaternion> Orientations;
+
     NYX_ALIGNAS_CACHE std::vector<real_t> Masses;
     NYX_ALIGNAS_CACHE std::vector<real_t> InvMasses;
+    NYX_ALIGNAS_CACHE std::vector<Mat3> Inertias;      // local space
+    NYX_ALIGNAS_CACHE std::vector<Mat3> InvInertias;   // local space
+
     NYX_ALIGNAS_CACHE std::vector<uint32_t> Active;
 
     friend class RigidbodySystem;
@@ -44,13 +48,15 @@ public:
     RigidbodySystem();
     ~RigidbodySystem() = default;
 
-    size_t addRigidbody(const Vec3& pos, const Vec3& vel, real_t mass);
+    size_t addRigidbody(const Vec3& pos, const Vec3& vel, real_t mass, const Mat3& inertia);
     void update(real_t dt);
+    void applyImpulse(size_t index, const Vec3& impulse, const Vec3& contactVector);
+
     NYX_FORCEINLINE const RigidbodyData& getData() const { return Data; }
     NYX_FORCEINLINE RigidbodyData& accessData() { return Data; }
 
 private:
-    void updateRigidbodies(real_t dt);
+    void integrate(real_t dt);
     void clearForces();
 
     RigidbodyData Data;
--- include/nyx/physics/scene/physics_world.h
+++ include/nyx/physics/scene/physics_world.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "nyx/core/base.h"
+#include "nyx/math/vec3.h"
+#include "nyx/math/mat3.h"
 #include "nyx/physics/rigidbody/rigidbody_system.h"
 
 namespace nyx {
@@ -9,14 +12,14 @@ public:
     PhysicsWorld();
     ~PhysicsWorld() = default;
 
-    size_t addRigidbody(const Vec3& pos, const Vec3& vel, real_t mass);
-    const RigidbodyData& getRigidbodyData() const { return Rb.getData(); }
-    RigidbodyData& accessRigidbodyData() { return Rb.accessData(); }
-
+    size_t addRigidbody(const Vec3& pos, const Vec3& vel, real_t mass, const Mat3& inertia);
     void update(real_t dt);
+
+    NYX_FORCEINLINE const RigidbodyData& getRigidbodyData() const { return Rb.getData(); }
+    NYX_FORCEINLINE RigidbodyData& accessRigidbodyData() { return Rb.accessData(); }
 
 private:
     RigidbodySystem Rb;
 };
 
-}  // namespace nyx
+} // namespace nyx
