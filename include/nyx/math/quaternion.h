cfb43462bc9ba237af468a1b59e258d4f60582c4
emomaxd <emreleno@gmail.com>
2025-05-19 22:49:42 +0300

    Separate quaternion impl

--- include/nyx/math/quaternion.h
+++ include/nyx/math/quaternion.h
@@ -1,59 +1,7 @@
 #pragma once
 
-#include "nyx/core/base.h"
-#include "nyx/math/vec3.h"
-
-namespace nyx {
-
-class Quaternion {
-public:
-    real_t w, x, y, z;
-
-    Quaternion() : w(1.0f), x(0.0f), y(0.0f), z(0.0f) {}
-    Quaternion(real_t w, real_t x, real_t y, real_t z) : w(w), x(x), y(y), z(z) {}
-    Quaternion(real_t w, const Vec3& v) : w(w), x(v.X), y(v.Y), z(v.Z) {}
-
-    static Quaternion identity() { return Quaternion(1.0f, 0.0f, 0.0f, 0.0f); }
-
-    Quaternion operator*(const Quaternion& other) const {
-        return Quaternion(
-            w * other.w - x * other.x - y * other.y - z * other.z,
-            w * other.x + x * other.w + y * other.z - z * other.y,
-            w * other.y - x * other.z + y * other.w + z * other.x,
-            w * other.z + x * other.y - y * other.x + z * other.w
-        );
-    }
-
-    Quaternion operator*(real_t scalar) const {
-        return Quaternion(w * scalar, x * scalar, y * scalar, z * scalar);
-    }
-
-    Quaternion operator+(const Quaternion& other) const {
-      return Quaternion(w + other.w, x + other.x, y + other.y, z + other.z);
-    }
-
-    Quaternion& operator+=(const Quaternion& other) {
-        w += other.w;
-        x += other.x;
-        y += other.y;
-        z += other.z;
-        return *this;
-    }
-    void normalize() {
-        real_t mag = std::sqrt(w * w + x * x + y * y + z * z);
-        if (mag > 0.0f) {
-            real_t invMag = 1.0f / mag;
-            w *= invMag;
-            x *= invMag;
-            y *= invMag;
-            z *= invMag;
-        }
-    }
-};
-
-// Non-member operator for float * Quaternion
-inline Quaternion operator*(real_t scalar, const Quaternion& q) {
-    return q * scalar; // Reuse the member operator
-}
-
-} // namespace nyx
+#ifdef NYX_USE_SSE4
+#include "quaternion_sse4.h"
+#else
+#include "quaternion_naive.h"
+#endif
